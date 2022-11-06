19d31c726c36a776b44215db6ace2902a6929e4b
Ashish Pundir <96501368+ashishpundir924@users.noreply.github.com>
2022-11-06 15:36:39 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,37 +1,33 @@
 # task 2 for program for shopping list
-option_1: str = input('To create a list type list, to call the saved list type call.')
+option_1: str = input('To create a list type list, to call the saved list type call. :- ')
 
-data_base = {"supermarket": ['mike', 'chocolate', 'egg', 'cigarette'], "shopping_from_india": [], "shopping_for_india": [], "shopping_from_poland": []}
+data_base = {"supermarket": ['mike', 'chocolate', 'egg', 'cigarette'], "shopping_from_india": [],
+             "shopping_for_india": [], "shopping_from_poland": []}
+
+
+def add_items(items):
+    return data_base.update({new_list: [items]})
+
+
+def call_f():
+    print('The total number of shopping list:- ', len(data_base))
+    for key in data_base.keys():
+        print(key)
+    else:
+        show_list = input("Type list name to display the items:")
+        for it in data_base[show_list]:
+            print(it)
+
 
 if option_1 == 'list':
     new_list = input("give a name of list:-")
     data_base[new_list] = []
-    nuber_of_element = int(input("enter the number of item you want to add :- "))
-    i = 0
-    for x in range(5):
 
-        data_base[x].update({new_list: [input("enter the item ")]})
+    for x in range(3):
+
+        items1 = input("enter i")
+        add_items(items1)
     else:
         print(data_base)
-
-
 elif option_1 == 'call':
-    print("in progress")
-    print(len(data_base))
-    print(data_base.keys())
-    show_list = input("Type list name to display the list:")
-    f_print = (show_list)
-    s_1 = data_base.fromkeys(f_print)
-    print(s_1)
-
-
-
-
-
-
-
-
-
-
-
-
+    call_f()
